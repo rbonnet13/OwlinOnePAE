e28d8aa5a35ca2a3d77dac8e0a324ac976c06aea
PyragriC <emile-prod44@hotmail.fr>
2018-02-13 15:46:02 +0100

    Suppression du res/menu/main.xml qui était inutilisé

--- app/src/main/java/owlinone/pae/session/RegisterActivity.java
+++ app/src/main/java/owlinone/pae/session/RegisterActivity.java
@@ -160,14 +160,6 @@ public class RegisterActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.main, menu);
-            return true;
-        }
-
         @Override
         public boolean onOptionsItemSelected(MenuItem item) {
     // Handle action bar item clicks here. The action bar will
