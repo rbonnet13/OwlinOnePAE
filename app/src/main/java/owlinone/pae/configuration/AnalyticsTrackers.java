3ca78b55f8ce7aeccdb83dd9716bc9cc116833d9
Julian <jfouillade.ir2018@esaip.org>
2018-02-21 10:45:57 +0100

    Retour d'analytics (à voir pour notifs)

--- app/src/main/java/owlinone/pae/configuration/AnalyticsTrackers.java
+++ app/src/main/java/owlinone/pae/configuration/AnalyticsTrackers.java
@@ -65,7 +65,7 @@ public final class AnalyticsTrackers {
             Tracker tracker;
             switch (target) {
                 case APP:
-                    tracker = GoogleAnalytics.getInstance(mContext).newTracker(R.xml.global_tracker);
+                    tracker = GoogleAnalytics.getInstance(mContext).newTracker(R.xml.gobal_tracker);
                     break;
                 default:
                     throw new IllegalArgumentException("Unhandled analytics target " + target);
