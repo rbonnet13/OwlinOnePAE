3208fc6b17b13fa685869fffbb18aa9748b5ec7d
Julian <jfouillade.ir2018@esaip.org>
2018-02-28 11:28:07 +0100

    Conducteur et autre

--- app/src/main/java/owlinone/pae/covoiturage/Conducteur.java
+++ app/src/main/java/owlinone/pae/covoiturage/Conducteur.java
@@ -7,6 +7,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -170,36 +171,44 @@ public class Conducteur extends AppCompatActivity {
                 strDate = obj.get("DATE_NOTIF");
                 strDestination = obj.get("DESTINATION_NOTIF");
 
-
                 AlertDialog.Builder builder = new AlertDialog.Builder(Conducteur.this);
-
                 builder.setTitle("Covoiturage");
                 builder.setIcon(R.drawable.owl_in_one_logo);
                 builder.setMessage("Accepter le covoiturage ?");
+
+                builder.setNegativeButton("OUI", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Notification push envoyée, vous pouvez envoyer un sms en effectuant un appui long sur l'item", Toast.LENGTH_LONG).show();
+                        dialog.dismiss();
+                    }
+                });
 
                 builder.setPositiveButton("NON", new DialogInterface.OnClickListener() {
 
                     public void onClick(DialogInterface dialog, int which) {
                         // Do nothing but close the dialog
-
+                        Toast.makeText(getApplicationContext(), "Refusé", Toast.LENGTH_LONG).show();
                         dialog.dismiss();
                     }
                 });
 
-                builder.setNegativeButton("OUI", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                        // Do nothing
-                        dialog.dismiss();
-                    }
-                });
                 AlertDialog alert = builder.create();
                 alert.show();
             }
-    });
-        //Sert pour l'appuie long (Entregistre le context du menu
-        registerForContextMenu(lv);
+        });
+
+        // Appui long sur un item
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int pos, long id) {
+                // on ouvre la fenêtre pour envoyer un sms
+                Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+                sendIntent.setData(Uri.parse("sms:" + strTel));
+                startActivity(sendIntent);
+                return true;
+            }
+        });
     }
 
     private class GetNotif extends AsyncTask<Void, Void, Void>
@@ -223,11 +232,14 @@ public class Conducteur extends AppCompatActivity {
                 {
                     Calendar c = Calendar.getInstance();
                     int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
-
-                    if(timeOfDay >= 0 && timeOfDay < 12){
+                    // on règle l'affichage des notifs
+                    if(timeOfDay >= 0 && timeOfDay < 10){
                         responseRequete = sh.performPostCall(AddressUrl.strNotifSchool, parameters);
-
-                    }else{
+                    }else if(timeOfDay >= 10 && timeOfDay < 12){
+                        responseRequete = sh.performPostCall(AddressUrl.strNotifHome, parameters);
+                    }else if(timeOfDay >= 12 && timeOfDay < 13){
+                        responseRequete = sh.performPostCall(AddressUrl.strNotifSchool, parameters);
+                    }else if(timeOfDay >= 13 && timeOfDay < 23){
                         responseRequete = sh.performPostCall(AddressUrl.strNotifHome, parameters);
                     }
                 }
@@ -260,7 +272,6 @@ public class Conducteur extends AppCompatActivity {
 
                     //Fonction pour utiliser agoTime
                     String agoTime = (String) DateUtils.getRelativeTimeSpanString(timeInMilliseconds, time, DateUtils.SECOND_IN_MILLIS);
-
                     HashMap<String, String> notification = new HashMap<>();
                     // adding each child node to HashMap key => value
                     notification.put("NOM_NOTIF", nom_notif);
@@ -290,19 +301,6 @@ public class Conducteur extends AppCompatActivity {
             lv.setAdapter(adapter);
         }
     }
-
-
-    public boolean onContextItemSelected(MenuItem item) {
-        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-        int index = info.position;
-        obj = (HashMap)lv.getItemAtPosition(index);
-
-        // Bla bla bla ....
-
-        return true;
-    }
-
-
 
     // Fonction appelée quand appuie sur la touche retour
     @Override
